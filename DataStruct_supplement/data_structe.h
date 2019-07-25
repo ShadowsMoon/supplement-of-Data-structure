f5377e4b8b9834f27bb9eaf1393efe8207fbfa8a
ShadowsMoon <2111707058@e.gzhu.edu.cn>
2019-07-25 21:38:54 +0800

    normal change

---
+++ DataStruct_supplement/data_structe.h
@@ -0,0 +1,10 @@
+#pragma once
+#ifndef  DATA01_SUM
+#define DATA01_SUM
+//-----------------------------------
+__int64 power_1(int n);
+__int64 power_2(int n);
+//-----------------------------------
+
+
+#endif
---
+++ DataStruct_supplement/function_sum.cpp
@@ -0,0 +1,15 @@
+#include"data_structe.h"
+
+//---------simple version-----
+__int64 power_1(int n) {
+	_int64 pow1 = 1;
+	while (n-- > 0) {
+		pow1<<= 1;
+	}
+	return pow1;
+}
+//-----------recursion----------------
+__int64 power_2(int n) {
+	return (n < 1) ? 1 : power_2(n - 1) <<1;
+
+}
