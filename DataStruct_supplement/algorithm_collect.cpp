0e6d4a70de1ed203317f683d21d0459f7ad0c2e3
ShadowsMoon <2111707058@e.gzhu.edu.cn>
2019-09-18 22:50:34 +0800

    new

---
+++ DataStruct_supplement/algorithm_collect.cpp
@@ -0,0 +1,58 @@
+//---典型编程题展示，求解-----//
+
+
+//----------------------------题1-----------------------------------------------------------//
+//从字符串string开始完整匹配子串sub，返回匹配到的字符个数。
+//sub中如果出现'?'表示可以匹配一到三个除'\0'以外的任意字符。
+//如果sub还有找不到匹配的字符，则说明不能完整匹配。
+//如果能完整匹配，返回匹配到的字符个数，如果有多种匹配方式，返回匹配字符数最少的那个，如果不能完整匹配，返回 - 1
+//
+//#include<vector>
+//#include<iostream>
+//#include<string>
+//#include<cstring>
+//using namespace std;
+////-----------------------------------------------------------------//
+//int main()
+//{
+//	char str[1005];
+//	char sub[105];
+//	cin >> str >> sub;
+//	int i, j;
+//	i = j = 0;
+//	int lena = strlen(str);
+//	int lenb = strlen(sub);
+//	int t = 0;
+//	int cnt = 0;
+//	while (i < lena&&j < lenb)
+//	{
+//		if (str[i] == sub[j])
+//		{
+//			i++;
+//			j++;
+//			cnt++;
+//			t = 0;
+//		}
+//		else
+//		{
+//			if (sub[j + 1] == str[i])
+//			{
+//				i++;
+//				j += 2;
+//				t = 0;
+//				cnt++;
+//			}
+//			else  if (sub[j] == '?'&&t < 3)
+//			{
+//				i++;
+//				cnt++;
+//				t++;
+//			}
+//			else break;
+//		}
+//	}
+//	if (j == lenb)cout << cnt << endl;
+//	else cout << -1 << endl;
+//	cin.get(); cin.get();
+//	return 0;
+//}
--- DataStruct_supplement/grammar_supplement.cpp
+++ DataStruct_supplement/grammar_supplement.cpp
@@ -208,6 +208,16 @@ using namespace std;
 //	return 0;
 //}
 
+//--------------------------------------------------//
+//argv 是 argument vector的缩写，表示传入main函数的参数序列或指针，并且第一个参数argv[0]一定是程序的名称，
+//并且包含了程序所在的完整路径，所以确切的说需要我们输入的main函数的参数个数应该是argc-1个；
+//argc 是 argument count的缩写，表示传入main函数的参数个数；
+void main(int argc, char *argv[])    
+{
+	for (int i = 0; i < argc; i++)
+	{
+		cout << i << ": " << argv[i] << endl;
+	}
+	system("pause");
+}
 
-
-//
